ff7597cc75df8560c16e866fc6a94425d08922eb
pc035860 <pc035860@gmail.com>
2025-12-29 15:42:50 +0800

    test(viewModels): remove obsolete commented tests
    - Remove 63 lines of commented-out test code that was no longer relevant
    - Remove unused import (buildChatMessageViewModels, formatCommentRuns)

--- app/tests/viewModels.test.ts
+++ app/tests/viewModels.test.ts
@@ -1,70 +1,7 @@
 import { strict as assert } from 'node:assert';
 import test from 'node:test';
 
-import { buildChatMessageViewModels, buildCommentViewModels } from '../src/source/utils/viewModels';
-import { formatCommentRuns } from '../src/source/utils/innertube/comments/pipeline';
-
-// test('buildCommentViewModels decodes HTML entities once', () => {
-//     const items = [
-//         {
-//             item: {
-//                 commentRenderer: {
-//                     authorText: { simpleText: 'Author' },
-//                     contentText: {
-//                         simpleText: '&amp;gt; Hello &amp;copy;'
-//                     }
-//                 }
-//             }
-//         }
-//     ];
-
-//     const [model] = buildCommentViewModels(items);
-//     assert.equal(model !== undefined, true, 'expected a comment model');
-//     assert.equal(model.contentHtml, '&gt; Hello &copy;');
-// });
-
-// test('buildCommentViewModels escape unexpected tags but keeps anchors', () => {
-//     const runs = [
-//         {text: '<test>unsafe</test><a href="/watch?v=1">link</a>' }
-//     ]
-    
-//     const formattedCommentContent = formatCommentRuns(runs, '123');
-//     const renderFullText = formattedCommentContent.renderFullText;
-
-//     assert.equal(formattedCommentContent !== undefined, true, 'expected a FormattedCommentContent');
-//     assert.equal(
-//         renderFullText,
-//         '&lt;test&gt;unsafe&lt;/test&gt;<a href="https://www.youtube.com/watch?v=1" rel="noopener noreferrer">link</a>'
-//     );
-// });
-
-// test('buildChatMessageViewModels decodes run text HTML entities once', () => {
-//     const items = [
-//         {
-//             item: {
-//                 replayChatItemAction: {
-//                     actions: [
-//                         {
-//                             addChatItemAction: {
-//                                 item: {
-//                                     liveChatTextMessageRenderer: {
-//                                         message: {
-//                                             runs: [{ text: '&amp;gt; Wow' }]
-//                                         }
-//                                     }
-//                                 }
-//                             }
-//                         }
-//                     ]
-//                 }
-//             }
-//         }
-//     ];
-
-//     const [model] = buildChatMessageViewModels(items);
-//     assert.equal(model !== undefined, true, 'expected a chat model');
-//     assert.equal(model.messageHtml, '&gt; Wow');
-// });
+import { buildCommentViewModels } from '../src/source/utils/viewModels';
 
 test('buildCommentViewModels extracts heartTooltip from creatorHeart.tooltip', () => {
     const items = [
